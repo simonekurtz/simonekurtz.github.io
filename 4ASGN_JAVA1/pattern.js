a0d92a36f531d9d3b1ab6108730bb62bd6faad33
simonekurtz <smkurtz6@gmail.com>
2015-03-06 16:43:42 +0000

    pattern update

---
+++ 4ASGN_JAVA1/pattern.js
@@ -0,0 +1,21 @@
+
+var red;
+red = 1;
+
+var blue;
+blue = red * 10;
+
+var yellow;
+yellow = blue * red / 50
+
+var green;
+green = 2;
+
+for (blue; blue <= 100; blue++) {
+   var result = blue * yellow;
+   console.log(result);
+}
+
+result * green
+
+green * blue == red * yellow
